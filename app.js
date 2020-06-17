ff5b74fd5acd5d9aab53c5fb88eb7e5336241de4
kimhansol <ekim569@gmail.com>
2020-06-17 12:20:22 +0900

    First Commit

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require('express')
-
+var path = require('path')
 var app = express()
+
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.get('/hello', (request, response)=>{
+    console.log(request)
+    response.render('hello.ejs')
+})
+
 var port = 8000;
 
-app.listen(port, function(){
+app.listen(port, ()=>{
     console.log(`Server is Running at http://localhost:8080`)
 
 })
