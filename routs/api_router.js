2335d12b097b96cbd2ec8110b591632469fbbca4
kimhansol <ekim569@gmail.com>
2020-06-18 17:33:36 +0900

    june18

--- routs/api_router.js
+++ routs/api_router.js
@@ -62,33 +62,29 @@ router.get('/topic/edit/:id', (req, res)=>{
 
 
 
-
-
-router.get('/topic/delete/:delid',(req,res)=>{
+router.get('/topic/delete/:delid' ,(req, res)=>{
     var delid = req.params.delid
-    db.query(`SELECT * FROM topic WHERE id ${delid}`, (err, results)=>{
-        var title = results[0].title
-        var description = results[0].description
-        var author = results[0].author
-
-        var sql_1 = `INSERT INTO deletelist (title, description, author) VALUES (${title} , ${description}, ${author})`
-        db.query(sql_1, (err, result_1)=>{
-    var sql = `DELETE FROM topic WHERE id = ${delid}`
-    if(err){
-        console.log(err)
-    }
-    })
-     db.query(sql, (err,results)=> {
+    db.query(`SELECT * FROM topic WHERE id=${delid}`,  (err, results)=>{
+    var title = results[0].title
+    var description =results[0].description
+    var author =results[0].author
+    var sql_1 = `INSERT INTO deletelist (title, description, author) VALUES (?, ?, ?)`;
+    var deleData = [title,description,author ]
+    db.query(sql_1,deleData, (err ,result_1 )=>{
+        var sql = `DELETE FROM topic WHERE  id=${delid}`
         if(err) {
             console.log(err)
         }
-        console.log(`delete : ${results}`)
-        res.redirect('/topic')
-    })
+        db.query(sql, (err, result)=>{
+            if(err) {
+                console.log(err)
+            }
+            console.log(result_1)
+            res.redirect('/topic')
+        })
+    }) 
+    })   
 })
-})
-
-
 
 
 router.post('/topic/:id/edit', (req, res)=>{
